cd0594bef82434670c0312816951d0254610765d
CodeCanna <fleabass4@gmail.com>
2019-06-27 19:23:01 -0600

    Added code to GitHub

---
+++ con_and_fun.js
@@ -0,0 +1,78 @@
+/*let equals = 1 === '1';
+console.log(equals);
+
+let gt = 5 > 1;
+console.log(gt);
+
+let lt = 2 < 10;
+console.log(lt);
+
+let gte = 5>=5;
+
+let lte = 4<=6
+
+let ne = 5!==3;
+*/
+/*
+let sA = 3.10;
+let sB = 4.40;
+
+let priceLower = sA < sB;
+
+console.log(priceLower);
+
+function comparePrices(sA, sB) {
+	let aLower = sA < sB;
+	if ( priceLower ) {
+		console.log("Price is lower.");
+	} else if (sA < sB) {
+		console.log("Price is higher.");
+	} else {
+		console.log("Price is equal.");
+	}
+}
+
+comparePrices(sA, sB);
+
+function squareNumber(number) {
+	let squared = number * number;
+	return squared;
+}
+
+let numSquare = squareNumber(3);
+ */
+/*
+let x = 10;
+
+function addNumbers(n, m, x) {
+	console.log(x);
+
+	let b;
+	if (1===1) {
+		b = 8;
+	}
+
+	console.log(b);
+	return n + m;
+}
+
+addNumbers(1, 2, 30);
+
+ */
+/*
+let arr = [1, 2, 3, 4, 5];
+
+for (i=0; i<arr.length; i++) {
+	console.log("I is now equal to: ", i);
+}
+
+ */
+/*
+let something = 0;
+
+while (something < 100) {
+	console.log(something);
+	something++;
+}
+
+ */
